2f4eb7029faa3aeaffe9692aec08b8d14526ab8b
Giorgi-dev <giorgivisual@gmail.com>
2024-01-04 21:18:04 +0100

    fix: image navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
   return (
     <nav className={styles['wrapper-navbar']}>
         <picture className={styles.navbar__logo} >
-          <Image  src={'/public/icta.png'} alt='Logo ICTA' width={154} height={134}/>
+          <Image  src={'/icta.png'} alt='Logo ICTA' width={154} height={134}/>
         </picture>
         <div className={styles.navbar__container}>
           <span className={styles['navbar__container--title']}>Instituto de Ciencia y Tecnología Agrícolas</span>
